b763c74d7c47e8ebb5d18b04fb98b3ddd6154c47
Terence Wu <twu429@gmail.com>
2013-12-10 12:24:10 -0800

    generates links to all of panos automatically using jquery

--- swfobject/test.js
+++ swfobject/test.js
@@ -1,32 +1,38 @@
-/*
- *  
- *  
- *
- *
- *
- */
 
+//global variable : krpano initialized when document is ready
 var krpano = undefined;
- 
-$( document ).ready(function() {krpano = document.getElementById("krpanoSWFObject");});
+var count = 0;
+$( document ).ready(function() {
+    krpano = document.getElementById("krpanoSWFObject");
+    createmorelinks();
+});
 
-function changepano(name) {
-    //some random scene to change to for testing
-    name = name || "virtualtourblank10"; 
-    
-    //scene_num + 1 = actual scene numbering based on the .xml file naming
-    //var krpano = document.getElementById("krpanoSWFObject");
-    krpano.call("loadpano('" + name + ".xml',null,MERGE,BLEND(1));");
-}
 
 function getpanonum(num) {
-    //some random scene to change to for testing
-    //num = num || "0";
-    //num = "virtualtourblank" + num;
+    //defaults to zeroth pano or sets to specified pano number
+    num = "virtualtourblank" + ( num || "0" ) + ".xml";
     
     //scene_num + 1 = actual scene numbering based on the .xml file naming
-    //var krpano = document.getElementById("krpanoSWFObject");
-    krpano.call("loadpano('" + "virtualtourblank23" + ".xml',null,MERGE,BLEND(1));");
+    krpano.call("loadpano('" + num + "',null,MERGE,BLEND(1));");
 }
 
+//generates all of the links to all of the panos
+function createmorelinks() {
+    for( var i = 0; i <= 52; i++) {
+	$( '#leftsidepanel' ).append('<a href=\"javascript:void(0);\" onclick=\"getpanonum(' + i +');\">Pano ' + i +'</a><br>'); 
+    }
+}
 
+// function loadfile() {
+//     $.get('stuff.txt', function(data) {
+// var lines = txt.responseText.split("\n");
+// for (var i = 0, len = lines.length; i < len; i++) {
+//     do something with lines[i];
+// }
+//     });
+// }
+
+function addtxt(string, location) {
+    $( location ).prepend("<p>"+ string + "</p>");
+    //$( location ).append("<p>"+ string + "</p>");
+}
