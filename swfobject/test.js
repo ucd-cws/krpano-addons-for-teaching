6d7af2b20b96fab685878cebbac60333eedbaf4b
Terence Wu <twu429@gmail.com>
2013-12-10 10:25:26 -0800

    side panel now able to change panos

--- swfobject/test.js
+++ swfobject/test.js
@@ -1,34 +1,32 @@
+/*
+ *  
+ *  
+ *
+ *
+ *
+ */
 
-var counter = 0;
-var worked = 0;
-//currently need a counter to make sure that the command has been run.
-function trythis() {
-    var krpano = document.getElementById("krpanoSWFObject");
-    if(krpano && krpano.get && counter <= 1) {
-	worked = krpano.set("view.fov", 10)
-	counter = counter + 1;
-    }
-}
-
-var lookat_interval = setInterval('trythis()', 21);
+var krpano = undefined;
  
+$( document ).ready(function() {krpano = document.getElementById("krpanoSWFObject");});
 
 function changepano(name) {
     //some random scene to change to for testing
     name = name || "virtualtourblank10"; 
     
     //scene_num + 1 = actual scene numbering based on the .xml file naming
-    var krpano = document.getElementById("krpanoSWFObject");
+    //var krpano = document.getElementById("krpanoSWFObject");
     krpano.call("loadpano('" + name + ".xml',null,MERGE,BLEND(1));");
 }
 
 function getpanonum(num) {
-    var krpano = document.getElementById("krpanoSWFObject");
     //some random scene to change to for testing
-    num = num || "0";
-    num = "virtualtourblank" + num;
+    //num = num || "0";
+    //num = "virtualtourblank" + num;
     
     //scene_num + 1 = actual scene numbering based on the .xml file naming
-    var krpano = document.getElementById("krpanoSWFObject");
-    krpano.call("loadpano('" + num + ".xml',null,MERGE,BLEND(1));");
+    //var krpano = document.getElementById("krpanoSWFObject");
+    krpano.call("loadpano('" + "virtualtourblank23" + ".xml',null,MERGE,BLEND(1));");
 }
+
+
