12da689316b197e21463fb8e72eff4534fb1145d
Terence Wu <twu429@gmail.com>
2014-01-08 17:20:24 -0800

    synchronizing and highlighting

--- swfobject/test.js
+++ swfobject/test.js
@@ -2,46 +2,37 @@
 //global variable : krpano initialized when document is ready
 //var krpano = undefined;
 var count = 0;
+var panonumber = 1;
 
 $( document ).ready(function() {
-    //krpano = document.getElementById("krpanoSWFObject");
-    createmorelinks();
+    $( '#leftsidepanel' ).prepend('<a href=\"javascript:void(0);\" onclick=\"testthis();\">show number</a><br>');
+	createmorelinks();
 
-    //$( '#leftsidepanel' ).prepend('<a href=\"javascript:void(0);\" onclick=\"correct();\">BLAH</a><br>');
 
-    $( '#leftsidepanel' ).prepend('<a href=\"javascript:void(0);\" onclick=\"testthis();\">shownumber</a><br>');
+	//this is needed to grab the value from the input box 
+    //and change the page number (currently the 0 and 52 are hard coded)
+	$( "#gotopageform" ).submit(function( event ) {
+		var value = parseInt($( "#gotopagebox" ).val());
+		if ( loadpanonum(value) ) {
+		}
+		else {
+			$( "#responsedd" ).text( "Must be 1 <= n <= 53!" ).show().fadeOut( 3000 );
+			event.preventDefault();
+		}
+		$("#gotopagebox").val(''); //clear value of box
+		return;
+	});
 
-    //below is testing the resizing functionality using jquery
-    //currently doesnt work.
-    var maxdocwidth = document.body.clientWidth
-
-    $('#leftsidepanel').resizable({
-	handles: 'e',
-	resize: function(event, ui){
-            var currentWidth = ui.size.width;
-            
-            // this accounts for padding in the panels + 
-            // borders, you could calculate this using jQuery
-            var padding = 12; 
-            
-            // this accounts for some lag in the ui.size value, if you take this away 
-            // you'll get some instable behaviour
-            $(this).width(currentWidth);
-            
-            // set the content panel width
-            $('#container').width(maxdocwidth - currentWidth - padding);            
-	}
-    });
-    
 });
+
+
+function krpano() {
+    return document.getElementById('krpanoSWFObject');
+}
 
 function testthis() {
     alert(getpanoid());
     krpano().call("testing();");
-}
-
-function alertme(value) {
-    alert(value);
 }
 
 //uses the name of the file to determine the scene #
@@ -50,7 +41,7 @@ function getpanoid() {
     var url = krpano().get("xml.url");
     url = url.replace(/\D+/g, ''); //remove all non-digits
     if(url) {
-	return parseInt(url) + 1;
+		return parseInt(url) + 1;
     }
     return 1;
 
@@ -58,41 +49,25 @@ function getpanoid() {
     // alert(JSON.stringify(scenelist, null, 4));
 }
 
-function callme() {
-    krpano().call("change_pano2();");
-}
-
-function krpano() {
-        return document.getElementById('krpanoSWFObject');
-}
-
 function loadpanonum(num) {
     //defaults to zeroth pano or sets to specified pano number
-    num = "virtualtourblank" + ( num || "0" ) + ".xml";
+	if( 1 <= num && num <= 53) {
+		panonumber = num;
+		num = "virtualtourblank" + (num - 1) + ".xml";
     
-    //scene_num + 1 = actual scene numbering based on the .xml file naming
-    krpano().call("loadpano('" + num + "',null,MERGE,BLEND(1));");
+        //scene_num + 1 = actual scene numbering based on the .xml file naming
+        krpano().call("loadpano('" + num + "',null,MERGE,BLEND(1));");
+		//CALL SOME FUNCTION TO HIGHLIGHT STUFF
+		return true;
+    }
+	return false;
 }
 
 //generates all of the links to all of the panos
 function createmorelinks() {
-    for( var i = 0; i <= 52; i++) {
-	$( '#leftsidepanel' ).append('<a href=\"javascript:void(0);\" onclick=\"loadpanonum(' + i +');\">Pano ' + (i + 1) +'</a><br>'); 
+    for( var i = 1; i <= 53; i++) {
+    $( '#leftsidepanel' ).append('<a id=\"' + "pano" + i + '\" href=\"javascript:void(0);\" onclick=\"loadpanonum(' + i +');\">Pano ' + i +'</a><br>');
     }
-   //TODO : Number of panos is hard-coded
-}
 
-// function loadfile() {
-//     $.get('stuff.txt', function(data) {
-// var lines = txt.responseText.split("\n");
-// for (var i = 0, len = lines.length; i < len; i++) {
-//     do something with lines[i];
-// }
-//     });
-// }
-
-//used for testing by terence
-function addtxt(string, location) {
-    $( location ).prepend("<p>"+ string + "</p>");
-    //$( location ).append("<p>"+ string + "</p>");
+   //TODO : Fix it so that Number of panos is not hard-coded
 }
