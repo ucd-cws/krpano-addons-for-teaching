52c8a6071bae9dcd88167001f4968f2759e4fcc5
Terence Wu <twu429@gmail.com>
2014-01-10 15:41:40 -0800

    highlighting works and synchronized panos (scenes)

--- swfobject/test.js
+++ swfobject/test.js
@@ -2,29 +2,43 @@
 //global variable : krpano initialized when document is ready
 //var krpano = undefined;
 var count = 0;
+var firstpanonum = 1; //even if it starts at 0 put 1.
+var lastpanonum = 53; //even if it ends at 52 put 53
 var panonumber = 1;
 
 $( document ).ready(function() {
     $( '#leftsidepanel' ).prepend('<a href=\"javascript:void(0);\" onclick=\"testthis();\">show number</a><br>');
 	createmorelinks();
 
+	//highlights the first pano as always (for now)
+	$('#pano' + panonumber).toggleClass('highlight');
 
 	//this is needed to grab the value from the input box 
-    //and change the page number (currently the 0 and 52 are hard coded)
+    //and change the page number (currently the 1 and 53 are hard coded)
 	$( "#gotopageform" ).submit(function( event ) {
 		var value = parseInt($( "#gotopagebox" ).val());
 		if ( loadpanonum(value) ) {
 		}
 		else {
-			$( "#responsedd" ).text( "Must be 1 <= n <= 53!" ).show().fadeOut( 3000 );
+			$( "#responsedd" ).text( "Must be " + firstpanonum +" <= n <= " + lastpanonum ).show().fadeOut( 3000 );
 			event.preventDefault();
 		}
 		$("#gotopagebox").val(''); //clear value of box
 		return;
 	});
-
+	checkpanonum();
+	
 });
 
+var checkpanonum = setInterval(function() {
+	if(getpanoid() != panonumber) {
+		updatepanohighlight(getpanoid());
+	}
+}, 100);
+
+function scrollto(id) {
+	location.hash = "#" + id;
+}
 
 function krpano() {
     return document.getElementById('krpanoSWFObject');
@@ -51,13 +65,13 @@ function getpanoid() {
 
 function loadpanonum(num) {
     //defaults to zeroth pano or sets to specified pano number
-	if( 1 <= num && num <= 53) {
-		panonumber = num;
+	if( firstpanonum <= num && num <= lastpanonum) {
+		updatepanohighlight(num);
 		num = "virtualtourblank" + (num - 1) + ".xml";
-    
+		
         //scene_num + 1 = actual scene numbering based on the .xml file naming
         krpano().call("loadpano('" + num + "',null,MERGE,BLEND(1));");
-		//CALL SOME FUNCTION TO HIGHLIGHT STUFF
+
 		return true;
     }
 	return false;
@@ -65,9 +79,17 @@ function loadpanonum(num) {
 
 //generates all of the links to all of the panos
 function createmorelinks() {
-    for( var i = 1; i <= 53; i++) {
-    $( '#leftsidepanel' ).append('<a id=\"' + "pano" + i + '\" href=\"javascript:void(0);\" onclick=\"loadpanonum(' + i +');\">Pano ' + i +'</a><br>');
-    }
 
-   //TODO : Fix it so that Number of panos is not hard-coded
+    for( var i = firstpanonum; i <= lastpanonum; i++) {
+		$( '#leftsidepanel' ).append('<a id=\"' + "pano" + i + '\" href=\"javascript:void(0);\" onclick=\"loadpanonum(' + i +');\">Pano ' + i +'</a><br>');
+    }
+}
+
+function updatepanohighlight(new_value) {
+	if(panonumber != new_value) {
+		$('#pano' + panonumber).toggleClass('highlight');
+		$('#pano' + new_value).toggleClass('highlight');
+		panonumber = new_value; //update global panonumber
+		scrollto("pano"+panonumber);
+	}	
 }
