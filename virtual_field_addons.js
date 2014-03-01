d92ed547122e9d738a0e27865a689d48ecc55f9d
twu <tewu@ucdavis.edu>
2014-02-28 16:06:29 -0800

    something changed

--- virtual_field_addons.js
+++ virtual_field_addons.js
@@ -18,8 +18,8 @@ $( document ).ready(function() {
 		classdata = new ClassData(response);
 		classdata.initialize();
 		
-	    hideAll();
-	    showAllHeaders();
+	    // hideAll();
+	    // showAllHeaders();
 		credits(); 
 		
 	}
